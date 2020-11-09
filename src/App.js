e1d8516d1e08d55c5922238cd46f0f5a1a7216aa
nic-ball <nicholasball147@gmail.com>
2020-11-09 19:51:26 +0000

    Added Components and began sorting out basic heirachy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Cv from './components/Cv';
+import Portfolio from './components/Portfolio';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import ContactMe from './components/ContactMe';
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className = 'App'>
+
+  <Header resumeData={resumeData}/>
+  <About resumeData={resumeData}/>
+  <Cv resumeData={resumeData}/>
+  <Portfolio resumeData={resumeData}/>
+  <Projects resumeData={resumeData}/>
+  <ContactMe resumeData={resumeData}/>
+  <Footer resumeData={resumeData}/>
+  </div>
   );
-}
+  }
+  }
 
 export default App;
---
+++ src/components/resumeData.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-multi-str */
+let resumeData = {
+  'imagebaseurl': 'https://avatars0.githubusercontent.com/u/22473672?s=460&u=2b077a4b47b6c29d2abd1e2b32ab2e5b1a1729a1&v=4',
+  'name': 'Nic',
+  'role': 'Full Stack Developer',
+  'linkedInId': 'nic-ball',
+  'roleDescription': 'I enjoy learning how to create scalable applications, using the vast amount of different tools available to a modern day software developer. In my spare time I like to play guitar, cook and cycle.',
+
+  'socialLinks': [
+    {
+      'name': 'github',
+      'url': 'https://github.com/nic-ball',
+      'className': 'fa-fa-github'
+    },
+    {
+      'name': 'linkedIn',
+      'url': 'https://www.linkedin.com/in/nic-ball/',
+      'className': 'fa-fa-linkedin'
+    }
+  ],
+
+  'aboutMe': 'I am currently using my time to update my skills and add new languages/tools to my workflow. \
+     As a self taught Full Stack Developer, find learning new skills and then being able to implement them one of the most satisfying things. \
+     When I was starting out there were many times of information overload and frustratio. Now that I have a good understanding of Software Development and the tools used, I find that I can process new information in a more succinct manner.'
+}
+
+export default resumeData;
